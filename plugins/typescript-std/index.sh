0d4ccef1141f2a23d0daf01e260c61c3b8d98a18
Olegzandr VD <alex@oresoftware.com>
2017-12-06 19:42:15 -0800

    pdev:set

--- plugins/typescript-std/index.sh
+++ plugins/typescript-std/index.sh
@@ -4,12 +4,12 @@ echo "running transform process, using plugin with name '$(basename `dirname $0`
 
 WHICH_TSC=$(which tsc);
 if [[ -z ${WHICH_TSC} ]]; then
-  npm install -g typescript
+    npm install -g typescript
 fi
 
 WHICH_SUMAN_TOOLS=$(which suman-tools);
 if [[ -z ${WHICH_SUMAN_TOOLS} ]]; then
-  npm install -g suman-tools
+    npm install -g suman-tools
 fi
 
 for x in $(suman-tools --extract-json-array=${SUMAN_TEST_PATHS}); do
@@ -22,9 +22,11 @@ for x in $(suman-tools --extract-json-array=${SUMAN_TEST_PATHS}); do
     else
         echo "we must transpile file."
         OUT_DIR="$(dirname $(dirname ${x}))/@target"
+        mkdir -p "$OUT_DIR"
         echo "x => $x"
         echo "SUMAN_TARGET => ${SUMAN_TARGET}"
-        tsc ${x} --noResolve --outDir "${OUT_DIR}" > /dev/null
+        echo "OUT_DIR => ${OUT_DIR}"
+        tsc ${x} --noResolve --outDir "${OUT_DIR}" # > /dev/null
         chmod -R 777 "${OUT_DIR}"
     fi
 
