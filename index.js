c546c1adfd9f49d2a3962a48daae6360f336b619
Operations Research Engineering Software+ <alex@oresoftware.com>
2018-09-29 16:48:02 -0700

    ores/auto-commit => 'set'

--- index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-/////////////////////////////
-
-console.error(new Error('This file does nothing, no reason to load it.'));
---
+++ lib/index.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+
+export const r2gSmokeTest = () => {
+   return true;
+};
